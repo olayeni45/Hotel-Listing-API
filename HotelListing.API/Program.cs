cb32f4811dd9bfb62f02d9687cdfda9ced6d6045
Olayeni Anifowose <olayeni45@gmail.com>
2025-05-26 12:04:45 +0100

    feat: serilog

--- HotelListing.API/Program.cs
+++ HotelListing.API/Program.cs
@@ -1,5 +1,6 @@
 using HotelListing.API.Data;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,9 +13,20 @@ builder.Services.AddDbContext<HotelListingDbContext>(options =>
 });
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", x => x.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
+});
+
+builder.Host.UseSerilog((ctx, lc) =>
+{
+    lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration);
+});
 
 var app = builder.Build();
 
@@ -25,7 +37,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseSerilogRequestLogging();
+
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
